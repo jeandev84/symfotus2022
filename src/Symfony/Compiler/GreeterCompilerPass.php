e5af1c016b488fb687936d3133e59aef8a960ebe
Jean-Claude <jeanyao@ymail.com>
2023-01-08 23:03:33 +0300

    Create GreeterCompilerPass

---
+++ src/Symfony/Compiler/GreeterCompilerPass.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Symfony\Compiler;
+
+use App\Service\MessageService;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+
+// AUTO INJECT greeter services
+class GreeterCompilerPass implements CompilerPassInterface
+{
+
+    public function process(ContainerBuilder $container)
+    {
+         if (! $container->has(MessageService::class)) {
+              return;
+         }
+
+         $messageService  = $container->findDefinition(MessageService::class);
+         $greeterServices = $container->findTaggedServiceIds('app.greeter_service');
+
+         foreach ($greeterServices as $id => $tags) {
+             $messageService->addMethodCall('addGreeter', [new Reference($id)]);
+         }
+
+         // это тоже самое писать в разделе "services" внутри файла./config/services.yaml
+         // services:
+         //   ...
+         //   MessageService:
+         //     calls:
+         //       -[addGreeter, ['@cite_formatter']]
+    }
+}
