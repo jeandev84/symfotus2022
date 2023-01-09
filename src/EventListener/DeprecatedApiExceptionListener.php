dca64458674debedc153d212b6d12257ada513ea
Jean-Claude <jeanyao@ymail.com>
2023-01-09 07:58:50 +0300

    HTTP Foundation

--- src/EventListener/DeprecatedApiExceptionListener.php
+++ src/EventListener/DeprecatedApiExceptionListener.php
@@ -1,7 +1,62 @@
 <?php
 namespace App\EventListener;
 
+use App\Exception\DeprecatedApiException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
 class DeprecatedApiExceptionListener
 {
+      public function onKernelException(ExceptionEvent $event): void
+      {
+            $exception = $event->getThrowable();
 
+            $response = new Response();
+            $response->setContent($exception->getMessage());
+
+            if ($exception instanceof DeprecatedApiException) {
+                $response->setStatusCode(Response::HTTP_GONE);
+            } else {
+                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+
+            $event->setResponse($response);
+      }
+
+
+
+      /*
+      public function __invoke(ExceptionEvent $event)
+      {
+          $exception = $event->getThrowable();
+
+          $response = new Response();
+          $response->setContent($exception->getMessage());
+
+          if ($exception instanceof DeprecatedApiException) {
+              $response->setStatusCode(Response::HTTP_GONE);
+          } else {
+              $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+          }
+
+          $event->setResponse($response);
+      }
+
+
+    public function onKernelExceptionMyCustomMethod(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        $response = new Response();
+        $response->setContent($exception->getMessage());
+
+        if ($exception instanceof DeprecatedApiException) {
+            $response->setStatusCode(Response::HTTP_GONE);
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
+    }
+    */
 }
