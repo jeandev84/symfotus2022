96da6e1cb36c84f9cf6dc79e4fbc9d37d0b90278
Яо Куасси Жан-Клод <jeanyao@ymail.com>
2023-10-31 22:45:52 +0300

    Bundles and Packages

---
+++ bundles/FeedBundle/DTO/SendNotificationDTO.php
@@ -0,0 +1,17 @@
+<?php
+namespace FeedBundle\DTO;
+
+class SendNotificationDTO
+{
+    private array $payload;
+
+    public function __construct(int $userId, string $text)
+    {
+        $this->payload = ['userId' => $userId, 'text' => $text];
+    }
+
+    public function toAMQPMessage(): string
+    {
+        return json_encode($this->payload);
+    }
+}
---
+++ bundles/FeedBundle/Entity/Feed.php
@@ -0,0 +1,78 @@
+<?php
+namespace FeedBundle\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+#[ORM\Table(name: 'feed')]
+#[ORM\UniqueConstraint(columns: ['reader_id'])]
+#[ORM\Entity]
+class Feed
+{
+    #[ORM\Column(name: 'id', type: 'bigint', unique:true)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    private int $id;
+
+    #[ORM\Column(name: 'reader_id', type: 'bigint', nullable: false)]
+    // тут была связь с User который остается в приложении поэтому указываем просто $readerId
+    private int $readerId;
+
+    #[ORM\Column(type: 'json', nullable: true)]
+    private ?array $tweets;
+
+    #[ORM\Column(name: 'created_at', type: 'datetime', nullable: false)]
+    #[Gedmo\Timestampable(on: 'create')]
+    private DateTime $createdAt;
+
+    #[ORM\Column(name: 'updated_at', type: 'datetime', nullable: false)]
+    #[Gedmo\Timestampable(on: 'update')]
+    private DateTime $updatedAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getReaderId(): int
+    {
+        return $this->readerId;
+    }
+
+    public function setReaderId(int $readerId): void
+    {
+        $this->readerId = $readerId;
+    }
+
+    public function getTweets(): ?array
+    {
+        return $this->tweets;
+    }
+
+    public function setTweets(?array $tweets): void
+    {
+        $this->tweets = $tweets;
+    }
+
+    public function getCreatedAt(): DateTime {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(): void {
+        $this->createdAt = new DateTime();
+    }
+
+    public function getUpdatedAt(): DateTime {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(): void {
+        $this->updatedAt = new DateTime();
+    }
+}
