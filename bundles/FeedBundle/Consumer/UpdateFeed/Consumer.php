7c69d631a6bd9c697906831c317bd8b1376ff6ed
Яо Куасси Жан-Клод <jeanyao@ymail.com>
2023-10-31 21:37:25 +0300

    FeedBundle

---
+++ bundles/FeedBundle/Consumer/UpdateFeed/Consumer.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace FeedBundle\Consumer\UpdateFeed;
+
+use StatsdBundle\Client\StatsdAPIClient;
+use FeedBundle\Consumer\UpdateFeed\Input\Message;
+use FeedBundle\DTO\SendNotificationDTO;
+use FeedBundle\Service\AsyncService;
+use FeedBundle\Service\FeedService;
+use Doctrine\ORM\EntityManagerInterface;
+use JsonException;
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class Consumer implements ConsumerInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    private ValidatorInterface $validator;
+
+    private FeedService $feedService;
+
+    private AsyncService $asyncService;
+
+    private StatsdAPIClient $statsdAPIClient;
+
+    private string $key;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator,
+        FeedService $feedService,
+        AsyncService $asyncService,
+        StatsdAPIClient $statsdAPIClient,
+        string $key
+    )
+    {
+        $this->entityManager = $entityManager;
+        $this->validator = $validator;
+        $this->feedService = $feedService;
+        $this->asyncService = $asyncService;
+        $this->statsdAPIClient = $statsdAPIClient;
+        $this->key = $key;
+    }
+
+    public function execute(AMQPMessage $msg): int
+    {
+        try {
+            $message = Message::createFromQueue($msg->getBody());
+            $errors = $this->validator->validate($message);
+            if ($errors->count() > 0) {
+                return $this->reject((string)$errors);
+            }
+        } catch (JsonException $e) {
+            return $this->reject($e->getMessage());
+        }
+
+        $tweetDTO = $message->getTweetDTO();
+        $this->feedService->putTweet($tweetDTO, $message->getFollowerId());
+        $notificationMessage = (new SendNotificationDTO($message->getFollowerId(), $tweetDTO->getText()))->toAMQPMessage();
+        $this->asyncService->publishToExchange(
+            AsyncService::SEND_NOTIFICATION,
+            $notificationMessage,
+            $message->getPreferred()
+        );
+
+        $this->statsdAPIClient->increment($this->key);
+        $this->entityManager->clear();
+        $this->entityManager->getConnection()->close();
+
+        return self::MSG_ACK;
+    }
+
+    private function reject(string $error): int
+    {
+        echo "Incorrect message: $error";
+
+        return self::MSG_REJECT;
+    }
+}
---
+++ bundles/FeedBundle/Consumer/UpdateFeed/Input/Message.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace FeedBundle\Consumer\UpdateFeed\Input;
+
+use FeedBundle\DTO\TweetDTO;
+use Symfony\Component\Validator\Constraints;
+
+final class Message
+{
+    private TweetDTO $tweetDTO;
+
+    #[Constraints\Regex('/^\d+$/')]
+    private int $followerId;
+
+    private string $preferred;
+
+    public static function createFromQueue(string $messageBody): self
+    {
+        $message = json_decode($messageBody, true, 512, JSON_THROW_ON_ERROR);
+        $result = new self();
+        $result->tweetDTO = new TweetDTO((int)$message['id'], $message['author'], $message['text'], $message['createdAt']);
+        $result->followerId = $message['followerId'];
+        $result->preferred = $message['preferred'];
+
+        return $result;
+    }
+
+    public function getTweetDTO(): TweetDTO
+    {
+        return $this->tweetDTO;
+    }
+
+    public function getFollowerId(): int
+    {
+        return $this->followerId;
+    }
+
+    public function getPreferred(): string
+    {
+        return $this->preferred;
+    }
+}
