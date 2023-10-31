9b463a900a1ce2c02124b326f0a10d0b97650703
Яо Куасси Жан-Клод <jeanyao@ymail.com>
2023-10-31 03:58:53 +0300

    Injection api-client

--- src/Controller/Api/SaveUser/v5/Controller.php
+++ src/Controller/Api/SaveUser/v5/Controller.php
@@ -1,50 +1,51 @@
 <?php
 namespace App\Controller\Api\SaveUser\v5;
 
-use App\Controller\Common\ErrorResponseTrait;
 use App\Controller\Api\SaveUser\v5\Input\SaveUserDTO;
-use App\Entity\User;
-use App\Manager\UserManager;
-use FOS\RestBundle\Context\Context;
+use App\Controller\Common\ErrorResponseTrait;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\Annotations\RequestParam;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
-
+use App\Controller\Api\SaveUser\v5\Output\UserIsSavedDTO;
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 class Controller extends AbstractFOSRestController
 {
-
     use ErrorResponseTrait;
 
+    private SaveUserManager $saveUserManager;
 
-    protected SaveUserManager $saveUserManager;
-
-
-
-    /**
-     * @param SaveUserManager $userManager
-    */
     public function __construct(SaveUserManager $saveUserManager)
     {
         $this->saveUserManager = $saveUserManager;
     }
 
-
-
-
     #[Rest\Post(path: '/api/v5/save-user')]
+    /**
+     * @OA\Post(
+     *     operationId="addUser",
+     *     tags={"Пользователи"},
+     *     @OA\RequestBody(
+     *         description="Input data format",
+     *         @OA\JsonContent(ref=@Model(type=SaveUserDTO::class))
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(ref=@Model(type=UserIsSavedDTO::class))
+     *     )
+     * )
+     */
     public function saveUserAction(SaveUserDTO $request, ConstraintViolationListInterface $validationErrors): Response
     {
-         if ($validationErrors->count()) {
-             $view = $this->createValidationErrorResponse(Response::HTTP_BAD_REQUEST, $validationErrors);
-             return $this->handleView($view);
-         }
-
-         $user = $this->saveUserManager->saveUser($request);
-         [$data, $code] = ($user->id === null) ? [['success' => false], 400] : [['user' => $user], 200];
-         return $this->handleView($this->view($data, $code));
+        if ($validationErrors->count()) {
+            $view = $this->createValidationErrorResponse(Response::HTTP_BAD_REQUEST, $validationErrors);
+            return $this->handleView($view);
+        }
+        $user = $this->saveUserManager->saveUser($request);
+        [$data, $code] = ($user->id === null) ? [['success' => false], 400] : [['user' => $user], 200];
+        return $this->handleView($this->view($data, $code));
     }
 }
--- src/Controller/Api/SaveUser/v5/Input/SaveUserDTO.php
+++ src/Controller/Api/SaveUser/v5/Input/SaveUserDTO.php
@@ -2,45 +2,48 @@
 namespace App\Controller\Api\SaveUser\v5\Input;
 
 use App\Entity\Traits\SafeLoadFieldsTrait;
-use App\Entity\User;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints as Assert;
+use OpenApi\Annotations as OA;
 
-
-# класс представления, то получаем из запроса
 class SaveUserDTO
 {
-
     use SafeLoadFieldsTrait;
 
-    #[Assert\NotBlank]
-    #[Assert\Type('string')]
-    #[Assert\Length(max: 32)]
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("string")
+     * @Assert\Length(max=32)
+     * @OA\Property(property="login", example="my_user")
+     */
     public string $login;
 
-
-    #[Assert\NotBlank]
-    #[Assert\Type('string')]
-    #[Assert\Length(max: 32)]
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("string")
+     * @Assert\Length(max=32)
+     * @OA\Property(property="password", example="my_pass")
+     */
     public string $password;
 
-
-    #[Assert\NotBlank]
-    #[Assert\Type('array')]
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("array")
+     * @OA\Property(property="roles", type="array", @OA\Items(type="string", example="ROLE_USER"))
+     */
     public array $roles;
 
-
-
-    #[Assert\NotBlank]
-    #[Assert\Type('numeric')]
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("numeric")
+     */
     public int $age;
 
-
-    #[Assert\NotBlank]
-    #[Assert\Type('boolean')]
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("bool")
+     * @OA\Property(property="isActive")
+     */
     public bool $isActive;
-
-
 
     public function getSafeFields(): array
     {
